5a6300748a247777b40bdb414897e4c52c663df7
Al Joseph <condinoaljoseph@gmail.com>
2020-12-30 11:42:23 +0800

    server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const app = express();
+require('./passport');
+
+const auth = require('./routes/auth');
+
+const PORT = 8080;
+
+app.use('/auth', auth);
+
+app.get('/', (req, res) => {
+	res.status(200).json({ msg: 'api' });
+});
+
+app.listen(PORT, () => console.log('running on port ' + PORT));
